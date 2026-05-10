7998cc21c37daaff97e7ab36a3b7cd8940eb7f9e
vazgensoghoyan <sogvazulchik@gmail.com>
2026-05-10 23:22:34 +0300

    1) defined ITape interface; 2) 2) simple .gitignore and basic files: README with the task (yet) and empty CMakeLists.txt; 3) renamed blank.yml -> testing.yml (file for github actions)

---
+++ include/tape/interface_tape.hpp
@@ -0,0 +1,28 @@
+#pragma once // tape/interface_tape.hpp
+
+#include <cstddef>
+#include <cstdint>
+
+namespace tape_sort::tape {
+
+class ITape {
+public:
+    virtual ~ITape() = default;
+
+    virtual int read() = 0;
+    virtual void write(int32_t value) = 0;
+
+    virtual void move_right() = 0;
+    virtual void move_left() = 0;
+
+    virtual void rewind() = 0; // перемотка ленты в начало
+
+    virtual bool can_move_right() const = 0;
+    virtual bool can_move_left() const = 0;
+
+    virtual size_t position() const = 0; // может, понадобится
+
+    virtual size_t size() const = 0;
+};
+
+} // namespace tape_sort::tape

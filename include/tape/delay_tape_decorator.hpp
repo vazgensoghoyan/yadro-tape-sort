6025dbee5555a2974c2e75a6b90cff60656310da
vazgensoghoyan <sogvazulchik@gmail.com>
2026-05-14 10:09:53 +0300

    Delay decorator for ITape objects: takes config and adds 'sleep_for' calls

---
+++ include/tape/delay_tape_decorator.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+#include <chrono>
+#include <thread>
+
+#include "tape/interface_tape.hpp"
+#include "config/config_structs.hpp"
+
+namespace tape_sort::tape {
+
+class DelayTapeDecorator final : public ITape {
+public:
+    DelayTapeDecorator(ITape& tape,
+                       const config::TapeConfig& config);
+
+    ~DelayTapeDecorator() override = default;
+
+    int32_t read() override;
+    void write(int32_t value) override;
+
+    void move_right() override;
+    void move_left() override;
+
+    void rewind() override;
+
+    bool can_move_right() const override;
+    bool can_move_left() const override;
+
+private:
+    static void sleep_ms(int ms);
+
+private:
+    ITape& tape_;
+    config::TapeConfig config_;
+};
+
+} // namespace tape_sort::tape
---
+++ src/tape/delay_tape_decorator.cpp
@@ -0,0 +1,49 @@
+#include "tape/delay_tape_decorator.hpp"
+
+#include <thread>
+#include <chrono>
+
+using namespace tape_sort::tape;
+
+DelayTapeDecorator::DelayTapeDecorator(ITape& tape, const config::TapeConfig& config)
+    : tape_(tape)
+    , config_(config)
+{}
+
+void DelayTapeDecorator::sleep_ms(int ms) {
+    if (ms > 0)
+        std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+}
+
+int DelayTapeDecorator::read() {
+    sleep_ms(config_.read_delay_ms);
+    return tape_.read();
+}
+
+void DelayTapeDecorator::write(int32_t value) {
+    sleep_ms(config_.write_delay_ms);
+    tape_.write(value);
+}
+
+void DelayTapeDecorator::move_right() {
+    sleep_ms(config_.move_delay_ms);
+    tape_.move_right();
+}
+
+void DelayTapeDecorator::move_left() {
+    sleep_ms(config_.move_delay_ms);
+    tape_.move_left();
+}
+
+void DelayTapeDecorator::rewind() {
+    sleep_ms(config_.rewind_delay_ms);
+    tape_.rewind();
+}
+
+bool DelayTapeDecorator::can_move_right() const {
+    return tape_.can_move_right();
+}
+
+bool DelayTapeDecorator::can_move_left() const {
+    return tape_.can_move_left();
+}
